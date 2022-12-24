e02b5c9abff3faa42bfa07d0788a333e55915e29
Jungmin Kim <jmkimm00@icloud.com>
2022-12-25 02:03:42 +0900

    비디오 업로드 폼 만들기1

---
+++ client/src/components/App.js
@@ -0,0 +1,33 @@
+import React, { Suspense } from 'react';
+import { Route, Switch } from "react-router-dom";
+import Auth from "../hoc/auth";
+// pages for this product
+import LandingPage from "./views/LandingPage/LandingPage.js";
+import LoginPage from "./views/LoginPage/LoginPage.js";
+import RegisterPage from "./views/RegisterPage/RegisterPage.js";
+import NavBar from "./views/NavBar/NavBar";
+import Footer from "./views/Footer/Footer";
+import VideoUploadPage from "./views/VideoUploadPage/VideoUploadPage.js"
+
+//null   Anyone Can go inside
+//true   only logged in user can go inside
+//false  logged in user can't go inside
+
+function App() {
+  return (
+    <Suspense fallback={(<div>Loading...</div>)}>
+      <NavBar />
+      <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
+        <Switch>
+          <Route exact path="/" component={Auth(LandingPage, null)} /> //아무나 들어갈 수 있음
+          <Route exact path="/login" component={Auth(LoginPage, false)} /> //로그인한 사람은 못 들어감
+          <Route exact path="/register" component={Auth(RegisterPage, false)} />
+          <Route exact path="/video/upload" component={Auth(VideoUploadPage, true)} /> //로그인한 사람만 들어갈 수 잇음
+        </Switch>
+      </div>
+      <Footer />
+    </Suspense>
+  );
+}
+
+export default App;
---
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { Typography, Button, Form, message, Input, Icon } from 'antd';
+import Dropzone from 'react-dropzone';
+
+const { TextArea } = Input;
+const { Title } = Typography;
+
+function VideoUploadPage() {
+    return ( 
+        <div style={{maxWidth: '700px', margin: '2rem auto'}}>
+            <div style = {{textAlign: 'center', marginBottom: '2rem'}}>
+                <Title level={2}>Uploaod Video</Title>
+            </div>
+
+            <Form onSubmit>
+                <div style={{display:'flex', justifyContent:'space-between'}}> 
+                    {/*Drop zone*/}
+                    <Dropzone
+            onDrop
+            multiple //한번에 파일을 2개이상올릴껀지
+            maxSize//최대사이즈 조절
+          >
+            {({ getRootProps, getInputProps }) => (
+              <div
+                style={{
+                    width: '300px',
+                    height: '240px', 
+                    border: '1px solid lightgray', 
+                    display: 'flex',
+                    alignItems: 'center', 
+                    justifyContent:'center'}} { ...getRootProps()}>
+                            <input {...getInputProps()} />
+                            <Icon type='plus' style={{fontsize: '3rem'}} />
+                        </div>
+                        )}
+                    </Dropzone>
+                    {/*Thumbnail*/}
+                    <div>
+                        <img src alt />
+                    </div>
+                </div>
+
+                <br />
+                <br />
+                <label>Title</label>
+                <Input
+                    onChange
+                    value
+                />
+                <br />
+                <br />
+                <label>Description</label>
+                <TextArea
+                    onChange
+                    value
+                />
+                <br />  
+                <br />
+
+                <select onChange> 
+                    <option key value></option>
+                </select>
+                <br />  
+                <br />
+                <select onChange> 
+                    <option key value></option>
+                </select>
+                <br />  
+                <br />
+                <Button type='primary' size='large' onClick> 
+                    Submit
+                </Button>
+
+            </Form>
+        </div>
+    )
+}
+
+export default VideoUploadPage
