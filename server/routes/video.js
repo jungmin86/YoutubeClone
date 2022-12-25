8404eb4ee19c9db13b6528e03f9ef499d04789c3
Jungmin Kim <jmkimm00@icloud.com>
2022-12-25 21:40:11 +0900

    Multer로 노드 서버에 mp4 저장하기

--- server/routes/video.js
+++ server/routes/video.js
@@ -6,23 +6,27 @@ const { auth } = require("../middleware/auth");
 const multer = require('multer');
 
 //Storage Multer Config
-let storage = multer.diskStorage({
+const storage = multer.diskStorage({
     destination:  (req, file, cb) => {
         cb(null, 'uploads/')
     },
     filename:  (req, file, cb) => {
         cb(null, `${Date.now()}_${file.originalname}`)
-    },
-    fileFilter: (req, file, cb) => {
-        const ext = path.extname(file.originalname)
-        if (ext !== '.mp4') {
-            return cb(res.status(400).end('only mp4 is allowed'), false);
-        }
-        cb(null, true)
     }
 })
+const fileFilter = (req, file, cb) => {
+    const typeArray = file.mimetype.split('/');
+    const fileType = typeArray[1];
+        // const ext = path.extname(file.originalname)
+        console.log(fileType+"박스는 보도록")
+        if (fileType == 'mp4') {
+            cb(null, true);
+        }
+        else {cb({msg:'mp4 파일만 업로드 가능합니다.'}, false)}
+    } 
 
-const upload = multer({ storage: storage }).single("file")
+
+const upload = multer({ storage: storage, fileFilter: fileFilter}).single("file")
 
 //=================================
 //             Video
